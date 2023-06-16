0bc67aae40830d921c2c27b5ae1c5c850ce4b2a0
dev_muuo <dev_muuo@outlook.com>
2023-06-16 13:24:16 -0400

    All Hello World Tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * A program that prints 'Programming is like building a multilingual puzzle'
+ * Return: value  0 successfull compilation
+ */
+int main()
+{
+	puts("programming is like building a multilingual puzzle \n");
+	return (0);
+}
+
